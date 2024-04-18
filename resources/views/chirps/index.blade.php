8b0ef634b0c5d5e8c46922e3056711ec3c6dac84
siimpildre <113007373+siimpildre@users.noreply.github.com>
2024-04-18 18:56:17 +0300

    news text field larger in desktop

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -8,7 +8,7 @@
         <div>
             <form method="POST" action="{{ route('chirps.store') }}">
                 @csrf
-                <textarea name="message" placeholder="{{ __('Millest uudis räägib?') }}" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
+                <textarea name="message" placeholder="{{ __('Millest uudis räägib?') }}" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm lg:h-72">
                     {{ old('message') }}
                 </textarea>
                 <x-input-error :messages="$errors->get('message')" class="mt-2" />
