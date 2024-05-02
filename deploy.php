635f4a76dbbdd3f48b058c12ed516bd16e3aeed2
siimpildre <113007373+siimpildre@users.noreply.github.com>
2024-05-02 22:36:56 +0300

    deploy config

--- deploy.php
+++ deploy.php
@@ -3,16 +3,46 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
-// Config
+// Zone yhendus
 
-set('repository', 'alias dep='vendor/bin/dep'');
+set('application', 'SaareSpordiselts');
+set('remote_user', 'virt118418');
+set('http_user', 'virt118418');
+set('keep_release', 2);
 
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+host('ta22pildre.itmajakas.ee')
+    ->setHostname('ta22pildre.itmajakas.ee')
+    ->set('http_user','virt118418')
+    ->set('deploy_path','~/domeenid/www.ta22pildre.itmajakas.ee/saarespordiselts
+')
+    ->set('branch','main');
 
-// Hosts
+set('repository', 'git@github.com:siimpildre/SiimPildre-final-TA22.git');
 
+// tasks
+
+task('opcache:clear', function () {
+    run('killall php82-cgi || true');
+})->desc('Clear opcache');
+
+task('build:node', function() {
+    cd('{{release_path}}');
+    run('npm i');
+    run('npx vite build');
+    run('rm -fr node_modules');
+});
+
+task('deploy',[
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'build:node',
+    'deploy:publish',
+    'opcache:clear',
+    'artisan:cache:clear'
+]);
 
 // Hooks
 
