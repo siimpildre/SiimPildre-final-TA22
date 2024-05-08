c725b0452b80c0e374e8941beeb918167f91674b
siimpildre <113007373+siimpildre@users.noreply.github.com>
2024-05-08 09:36:53 +0300

    logos added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,15 +10,13 @@
         </div>
     </x-slot>
 
-    <div class="grid grid-cols-1 p-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="text-center grid grid-cols-1 p-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <a href="./teams" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
                 <div class="px-4 py-2  text-white">
                     <h2 class="text-lg font-bold">Meeskonnad</h2>
                 </div>
-                <div>
-                    <img class="block h-9 w-auto fill-current bg-white text-gray-600" src="/public/icons/team-add.svg" alt="team-icon">
-                </div>
                 <div class="px-4 py-2 bg-white">
+                    <x-team-add-logo class=" block h-9 w-auto fill-current bg-white text-gray-600" alt="team-add-logo"/>
                     <p>Vaata ja muuda meeskonna infot</p>
                 </div>
             </a>
@@ -28,7 +26,7 @@
                     <h2 class="text-lg font-bold">Mängijad</h2>
                 </div>
                 <div class="px-4 py-2 bg-white">
-                    <img class="block h-9 w-auto fill-current" src="/public/icons/jersey.svg" alt="jersey-icon">
+                    <x-jersey-logo class="block h-9 w-auto fill-current bg-white text-gray-600" alt="jersey-logo"/>
                     <p>Vaata ja muuda mängijate infot</p>
                 </div>
             </a>
@@ -38,7 +36,7 @@
                     <h2 class="text-lg font-bold">Ajakava</h2>
                 </div>
                 <div class="px-4 py-2 bg-white">
-                    <img class="block h-9 w-auto fill-current" src="/public/icons/schedule.svg" alt="schedule-icon">
+                    <x-schedule-logo class="block h-9 w-auto fill-current" alt="schedule-icon"/>
                     <p>Vaata ja muuda ajakava</p>
                 </div>
             </a>
@@ -48,7 +46,7 @@
                     <h2 class="text-lg font-bold">Uudised</h2>
                 </div>
                 <div class="px-4 py-2 bg-white">
-                    <img class="block h-9 w-auto fill-current" src="/public/icons/news.svg" alt="news-icon">
+                    <x-news-logo class="block h-9 w-auto fill-current" alt="news-icon"/>
                     <p>Vaata või loo uusi uudiseid</p>
                 </div>
             </a>
@@ -57,7 +55,7 @@
                     <h2 class="text-lg font-bold">Statistika</h2>
                 </div>
                 <div class="px-4 py-2 bg-white">
-                    <img class="block h-9 w-auto fill-current" src="/public/icons/statistics.svg" alt="statistics-icon">
+                    <x-statistics-logo class="block h-9 w-auto fill-current" alt="statistics-icon"/>
                     <p>Sisesta või muuda mängude statistikat</p>
                 </div>
             </a>
