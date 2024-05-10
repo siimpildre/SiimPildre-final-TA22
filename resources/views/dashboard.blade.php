a1f4e6026214509369cb7e0e7bac0d68b25164a7
siimpildre <113007373+siimpildre@users.noreply.github.com>
2024-05-10 13:40:48 +0300

    icons centered

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,59 +11,66 @@
     </x-slot>
 
     <div class="text-center grid grid-cols-1 p-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <a href="./teams" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
-                <div class="px-4 py-2  text-white">
-                    <h2 class="text-lg font-bold">Meeskonnad</h2>
-                </div>
-                <div class="px-4 py-2 bg-white">
-                    <x-team-add-logo class=" block h-9 w-auto fill-current bg-white text-gray-600" alt="team-add-logo"/>
-                    <p>Vaata ja muuda meeskonna infot</p>
-                </div>
-            </a>
+        <a href="./teams" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
+            <div class="px-4 py-2  text-white">
+                <h2 class="text-lg font-bold">Meeskonnad</h2>
+            </div>
+            <div class="flex justify-center px-4 py-2 bg-white">
+                <x-team-add-logo class=" block h-9 w-auto fill-current bg-white text-gray-600" alt="team-add-logo"/>
+            </div>
+            <div class="px-4 py-2 bg-white">
+                
+                <p>Vaata ja muuda meeskonna infot</p>
+            </div>
+        </a>
 
-            <a href="./players" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
-                <div class="px-4 py-2  text-white">
-                    <h2 class="text-lg font-bold">Mängijad</h2>
-                </div>
-                <div class="px-4 py-2 bg-white">
-                    <x-jersey-logo class="block h-9 w-auto fill-current bg-white text-gray-600" alt="jersey-logo"/>
-                    <p>Vaata ja muuda mängijate infot</p>
-                </div>
-            </a>
+        <a href="./players" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
+            <div class="px-4 py-2  text-white">
+                <h2 class="text-lg font-bold">Mängijad</h2>
+            </div>
+            <div class="flex justify-center px-4 py-2 bg-white">
+                <x-jersey-logo class="block h-9 w-auto fill-current bg-white text-gray-600" alt="jersey-logo"/>
+            </div>
+            <div class="px-4 py-2 bg-white">
+                <p>Vaata ja muuda mängijate infot</p>
+            </div>
+        </a>
 
-            <a href="./schedules" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
-                <div class="px-4 py-2 text-white">
-                    <h2 class="text-lg font-bold">Ajakava</h2>
-                </div>
-                <div class="px-4 py-2 bg-white">
-                    <x-schedule-logo class="block h-9 w-auto fill-current" alt="schedule-icon"/>
-                    <p>Vaata ja muuda ajakava</p>
-                </div>
-            </a>
+        <a href="./schedules" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
+            <div class="px-4 py-2 text-white">
+                <h2 class="text-lg font-bold">Ajakava</h2>
+            </div>
+            <div class="flex justify-center px-4 py-2 bg-white">
+                <x-schedule-logo class="block h-9 w-auto fill-current" alt="schedule-icon"/>
+            </div>
+            <div class="px-4 py-2 bg-white">
+                <p>Vaata ja muuda ajakava</p>
+            </div>
+        </a>
 
-            <a href="./chirps" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
-                <div class="px-4 py-2  text-white">
-                    <h2 class="text-lg font-bold">Uudised</h2>
-                </div>
-                <div class="px-4 py-2 bg-white">
-                    <x-news-logo class="block h-9 w-auto fill-current" alt="news-icon"/>
-                    <p>Vaata või loo uusi uudiseid</p>
-                </div>
-            </a>
-            <a href="./statistics" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
-                <div class="px-4 py-2 text-white">
-                    <h2 class="text-lg font-bold">Statistika</h2>
-                </div>
-                <div class="px-4 py-2 bg-white">
-                    <x-statistics-logo class="block h-9 w-auto fill-current" alt="statistics-icon"/>
-                    <p>Sisesta või muuda mängude statistikat</p>
-                </div>
-            </a>
-
+        <a href="./chirps" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
+            <div class="px-4 py-2  text-white">
+                <h2 class="text-lg font-bold">Uudised</h2>
+            </div>
+            <div class="flex justify-center px-4 py-2 bg-white">
+                <x-news-logo class="block h-9 w-auto fill-current" alt="news-icon"/>
+            </div>
+            <div class="px-4 py-2 bg-white">
+                <p>Loo uusi uudiseid</p>
+            </div>
+        </a>
+        <a href="./statistics" class="bg-orange-600 hover:bg-orange-700 shadow-lg rounded-lg overflow-hidden text-gray-600 hover:text-gray-800">
+            <div class="px-4 py-2 text-white">
+                <h2 class="text-lg font-bold">Statistika</h2>
+            </div>
+            <div class="flex justify-center px-4 py-2 bg-white">
+                <x-statistics-logo class="block h-9 w-auto fill-current" alt="statistics-icon"/>
+            </div>
+            <div class="px-4 py-2 bg-white">
+                <p>Sisesta või muuda mängude statistikat</p>
+            </div>
+        </a>
     </div>
-    
-
-
 </x-app-layout>
 
 <script>
